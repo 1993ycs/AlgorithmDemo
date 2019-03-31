79413a01a25303dfa969c266d3e8fa01a95c645f
1993ycs <yucs2017@163.com>
2019-03-31 15:29:40 +0800

    update

--- src/listdemo/ListDemo2.java
+++ src/listdemo/ListDemo2.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 
 /**
  * author:ycs
- * email: yucs2017@163.com
+ * email: yucs2017@163.com   '''
  * Date:2019/1/19
  * Time:19:17
  */
-
-
 public class ListDemo2 {
     public static void main(String[] args) {
-        HashMap map = new HashMap();
+        HashMap map = new HashMap(16);
         for (int i = 0; i < 10; i++) {
             map.put(i, i*i);
         }
+//        (((((((((((((SourceTree)))))))))))));
+
     }
 
 }
